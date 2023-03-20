f9b415590767ac36078d9d7fac0e145bcb52736f
Martha <ehidiamenmartha@yahoo.com>
2023-03-20 12:15:27 -0700

    Variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,11 +15,11 @@ int main(void)
 		{
 			if (x < y)
 			{
-				putchar((x / 10) + 0);
-				putchar((x % 10) + 0);
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
 				putchar(' ');
-				putchar((y / 10) + 0);
-				putchar((y % 10) + 0);
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
 				if (x != 98 || y != 99)
 				{
 					putchar(',');
