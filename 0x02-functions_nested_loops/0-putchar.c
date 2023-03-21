264ae5ee4228c29c6d98be3961b26936e22a3860
Martha <ehidiamenmartha@yahoo.com>
2023-03-21 06:29:42 -0700

    Functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Prints putchar followed by a new line
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('\n);
+	return (0);
+}
