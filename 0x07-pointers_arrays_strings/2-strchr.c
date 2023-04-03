f803fbff3469bbcee298dec19174dc5442a342e7
Martha <ehidiamenmartha@yahoo.com>
2023-04-03 07:28:07 -0700

    Locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,16 +8,16 @@
  * Return: NULL if char not found
  */
 
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char c)
 {
 	while (*s)
 	{
-		if (*s != c)
+		if (*s == c)
 			s++;
 		else
 			return (s);
 	}
-	if (c == '\0')
+	if (*s != '\0')
 		return (s);
 
 
