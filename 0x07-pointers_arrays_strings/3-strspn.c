e1248043a72cadf29c74a5d1bde9553a6d9f9d9b
Martha <ehidiamenmartha@yahoo.com>
2023-04-03 11:36:25 -0700

    Length of a prefix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -20,12 +20,12 @@ unsigned int _strspn(char *s, char *accept)
 			if (*s == accept[b])
 			{
 				a++;
-				break;
+
 			}
 			else if (accept[b + 1] == '\0')
 				return (a);
 		}
-		s++
+		s++;
 	}
 	return (a);
 }
