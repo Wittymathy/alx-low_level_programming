908c532ca58cdbdb09ff230134a9f4d8277effcc
Martha <ehidiamenmartha@yahoo.com>
2023-04-07 02:10:25 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,25 +12,15 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 		if (a % 15 == 0)
-		{
 			printf("FizzBuzz");
-		}
 		else if (a % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		else if (a % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		else
-		{
 			printf("%a", a);
-		}
 		if (a < 100)
-		{
 			printf(" ");
-		}
 	}
 	printf("\n");
 	return (0);
