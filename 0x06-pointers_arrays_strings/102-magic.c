12b44dabc5807a8e03c0ce9df7aa63a3b113957f
Martha <ehidiamenmartha@yahoo.com>
2023-04-07 22:47:06 -0700

    One line

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds one line of code
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	int b;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &b;
+
+	printf("a[2] = %d\n", a[2]);
+
+	return (0);
+}
