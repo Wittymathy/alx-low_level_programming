5b3ea3aee858e2fe9cb1f289d4982d539eb8a31f
Martha <ehidiamenmartha@yahoo.com>
2023-04-29 03:33:35 -0700

    performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,16 +17,14 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	i = 0;
-
-	while (i < 10)
+	while (ops[i].op)
 	{
-		if (s[0] == ops->op[i])
-			break;
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
 		i++;
 	}
 
-	return (ops[i / 2].f);
+	return (NULL);
 }
